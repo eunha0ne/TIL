604ad28dcea515f35169eae6f3cd8a16835053d5
gyuusik <gs.kim@birdview.kr>
2021-03-15 10:27:27 +0900

    Chore: 새로운 문서를 연결하고, 컨벤션을 정리하라

--- docs/.vuepress/sideBarList.js
+++ docs/.vuepress/sideBarList.js
@@ -1,75 +1,80 @@
 exports.sideBarList = [
-    {
-        title: 'AWS',
-        children: ['AWS/'],
-    },
-    {
-        title: 'books',
-        children: ['books/객체지향의-사실과-오해.md'],
-    },
-    {
-        title: 'Career',
-        children: ['Career/'],
-    },
-    {
-        title: 'CS',
-        children: ['CS/'],
-    },
-    {
-        title: 'CSS',
-        children: ['CSS/', 'CSS/media-query.md', 'CSS/pseudo.md'],
-    },
-    {
-        title: 'DevEnvironment',
-        children: [
-            'DevEnvironment/',
-            'DevEnvironment/NPM.md',
-            'DevEnvironment/NVM.md',
-            'DevEnvironment/Webpack.md',
-        ],
-    },
-    {
-        title: 'Git',
-        children: ['Git/Git.md'],
-    },
-    {
-        title: 'HTML',
-        children: ['HTML/', 'HTML/semantic-markup.md', 'HTML/tags.md'],
-    },
-    {
-        title: 'IDE',
-        children: ['IDE/', 'IDE/Webstorm.md'],
-    },
-    {
-        title: 'JavaScript',
-        children: [
-            'JavaScript/',
-            'JavaScript/DOM.md',
-            'JavaScript/fetch.md',
-            'JavaScript/intersection-observer.md',
-            'JavaScript/promise.md',
-            'JavaScript/UI-Event.md',
-            'JavaScript/window.md',
-        ],
-    },
-    {
-        title: 'Linux',
-        children: ['Linux/Linux.md', 'Linux/shell-script.md'],
-    },
-    {
-        title: 'macOS',
-        children: ['macOS/', 'macOS/git-auto-completion.md'],
-    },
-    {
-        title: 'TIR',
-        children: ['TIR/'],
-    },
-    {
-        title: 'Vim',
-        children: ['Vim/Vim.md'],
-    },
-    {
-        title: 'Vue',
-        children: ['Vue/Vue.md', 'Vue/vue-press.md'],
-    },
+  {
+    title: 'AWS',
+    children: ['AWS/', 'AWS/ELB'],
+  },
+  {
+    title: 'books',
+    children: ['books/객체지향의-사실과-오해.md'],
+  },
+  {
+    title: 'Career',
+    children: ['Career/'],
+  },
+  {
+    title: 'CS',
+    children: ['CS/'],
+  },
+  {
+    title: 'CSS',
+    children: ['CSS/', 'CSS/media-query.md', 'CSS/pseudo.md'],
+  },
+  {
+    title: 'DevEnvironment',
+    children: [
+      'DevEnvironment/',
+      'DevEnvironment/MonoRepo',
+      'DevEnvironment/NPM.md',
+      'DevEnvironment/NVM.md',
+      'DevEnvironment/Webpack.md',
+    ],
+  },
+  {
+    title: 'Git',
+    children: ['Git/Git.md'],
+  },
+  {
+    title: 'HTML',
+    children: ['HTML/', 'HTML/semantic-markup.md', 'HTML/tags.md'],
+  },
+  {
+    title: 'IDE',
+    children: ['IDE/', 'IDE/Webstorm.md'],
+  },
+  {
+    title: 'JavaScript',
+    children: [
+      'JavaScript/',
+      'JavaScript/DOM.md',
+      'JavaScript/fetch.md',
+      'JavaScript/intersection-observer.md',
+      'JavaScript/promise.md',
+      'JavaScript/UI-Event.md',
+      'JavaScript/window.md',
+    ],
+  },
+  {
+    title: 'Linux',
+    children: ['Linux/Linux.md', 'Linux/shell-script.md'],
+  },
+  {
+    title: 'macOS',
+    children: ['macOS/', 'macOS/git-auto-completion.md'],
+  },
+  {
+    title: 'Network',
+    children: ['Network/'],
+  },
+  {
+    title: 'TIR',
+    children: ['TIR/'],
+  },
+  {
+    title: 'Vim',
+    children: ['Vim/Vim.md'],
+  },
+  {
+    title: 'Vue',
+    children: ['Vue/Vue.md', 'Vue/vue-press.md'],
+  },
 ]
